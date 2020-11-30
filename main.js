13b556581b94b3c0912880ba1330ecdcb1caddc1
Arth Raval <connect.arth.me@gmail.com>
2020-11-30 12:00:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+function make2DArray(cols, rows) {
+  let arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
+}
+let grid;
+let cols;
+let rows;
+let slider;
+let resolution = 10;
+
+function setup() {
+  slider = createSlider(10,40, 10, 1); 
+  
+  resolution = slider.value();
+  var canvas = createCanvas(1920, 1080);
+  var sliderhold = slider
+  slider.parent('slider-holder');
+  canvas.parent('sketch-holder');
+
+  cols = width / resolution;
+  rows = height / resolution;
+
+  grid = make2DArray(cols, rows);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = floor(random(2));
+    }
+  }
+}
+function draw() {
+  background(18,18,18);
+  resolution = slider.value();
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * resolution;
+      let y = j * resolution;
+      if (grid[i][j] == 1) {
+        fill(255,0,77);
+        stroke(255,0,77);
+        rect(x, y, resolution - 1, resolution - 1);
+      }
+    }
+  }
+
+  let next = make2DArray(cols, rows);
+
+  // Compute next based on grid
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let state = grid[i][j];
+      // Count live neighbors!
+      let sum = 0;
+      let neighbors = countNeighbors(grid, i, j);
+
+      if (state == 0 && neighbors == 3) {
+        next[i][j] = 1;
+      } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+        next[i][j] = 0;
+      } else {
+        next[i][j] = state;
+      }
+    }
+  }
+  grid = next;
+}
+function countNeighbors(grid, x, y) {
+  let sum = 0;
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+      let col = (x + i + cols) % cols;
+      let row = (y + j + rows) % rows;
+      sum += grid[col][row];
+    }
+  }
+  sum -= grid[x][y];
+  return sum;
+}
